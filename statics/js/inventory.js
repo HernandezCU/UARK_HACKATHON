d2947232313cb9fd19f131b9ff5c5eecb666ee67
Rithyka Heng <rkheng@uark.edu>
2022-11-12 09:56:03 -0500

    get key and last uploaded image

--- statics/js/inventory.js
+++ statics/js/inventory.js
@@ -1,5 +1,44 @@
+function prep() {
+    //get api key
+    jFetch(`${location.href}api/key`, {}, (data) => {
+        setGlobalConstant("API_KEY", data.key);
+    });
+
+    //get image
+    bFetch(`${location.href}api/cdn/${stringDecode(document.cookie, ",")["key"]}`, {}, (data) => {
+        window["uploadedImageUrl"] = data;
+    });
+
+    let inte = setInterval(() => {
+        if(typeof(API_KEY) != "undefined" && typeof(uploadedImageUrl) != "undefined") {
+            clearInterval(inte);
+            main();
+        }
+    }, 100);
+}
 function main() {
+    //use quagga with image above to get upc
     //
+
+    //if upc found, use spoonacular search product by upc
+    //
+
+    //if no upc found, then use spoonacular image analysis
+    jFetch(IMG_ANAL, {
+        apiKey: API_KEY,
+        imageUrl: uploadedImageUrl
+    }, (data) => {
+        console.log(data);
+        //{ "status": "failure", "code": 400, "message": "Could not analyze food image" }
+    });
 }
 
-window.addEventListener("load", main);
+function setGlobalConstant(name, value) {
+    Object.defineProperty(window, name, {
+        value: value,
+        configurable: false,
+        writable: false
+    });
+}
+
+window.addEventListener("load", prep);
--- statics/js/utils.js
+++ statics/js/utils.js
@@ -1,5 +1,8 @@
-function stringDecode(input) {
-    let pairs = input.split(",");
+const   BY_UPC = `https://api.spoonacular.com/food/products/upc`,
+        IMG_ANAL = `https://api.spoonacular.com/food/images/analyze`;
+
+function stringDecode(input, delimiter) {
+    let pairs = input.split(delimiter);
     let output = {};
     for(let pair of pairs) {
         let [key, value] = pair.split("=");
@@ -7,17 +10,27 @@ function stringDecode(input) {
     }
     return output;
 }
-
-function stringEncode(input) {
+function stringEncode(input, delimiter) {
     let output = "";
     for(let [key, value] of Object.entries(input)) {
-        output += `${key}=${value},`;
+        output += `${key}=${value}${delimiter}`;
     }
     return output.length > 0 ? output.substring(0, output.length - 1) : output;
 }
 
-function pFetch(url, params, callback) {
-    fetch(`${url}?${stringEncode(params)}`)
+function bFetch(url, params, callback) {
+    fetch(`${url}?${stringEncode(params, "&")}`)
+    .then((res) => (res.blob()))
+    .then((data) => {
+        const imageObjectURL = URL.createObjectURL(data);
+        callback(imageObjectURL);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+}
+function jFetch(url, params, callback) {
+    fetch(`${url}?${stringEncode(params, "&")}`)
     .then((res) => (res.json()))
     .then((data) => {
         callback(data);
