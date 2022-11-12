833f9a90e9bbab662daebbc33aa422e970441d82
Carlos Hernandez <hernandezcu@jbu.edu>
2022-11-12 10:53:39 -0600

    img

--- main.py
+++ main.py
@@ -242,4 +242,4 @@ async def keys(request: fastapi.Request):
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
--- statics/js/upload.js
+++ statics/js/upload.js
@@ -1,13 +1,13 @@
 var input = document.getElementById("file"),
-    image = document.getElementById("img");
+	image = document.getElementById("img");
 
 function updateImg() {
-    let file = input.files[0],
-        reader = new FileReader();
-    reader.onload = (ev) => {
-        image.src = ev.target.result;
-    };
-    reader.readAsDataURL(file);
+	let file = input.files[0],
+		reader = new FileReader();
+	reader.onload = (ev) => {
+		image.src = ev.target.result;
+	};
+	reader.readAsDataURL(file);
 }
 
-input.addEventListener("onchange", updateImg);
+input.addEventListener("change", updateImg);
